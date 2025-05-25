8409f9e63011da7705f1c6f210f3ef1acc3de5d5
Evan <evan.froc35@gmail.com>
2025-05-25 11:46:45 +0200

    push

--- src/graphic/GUI.java
+++ src/graphic/GUI.java
@@ -1,18 +1,9 @@
 package graphic;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-
-import javafx.event.ActionEvent;
 
 public class GUI extends JFrame{
     
